6367e5e75f95d0eab016bcc67dfe944fe4acf574
ash92596 <ashleigh.presley@faulkner.edu>
2018-10-30 17:09:04 -0500

    Step 3
    Create a constructor that takes in fileName, title, description, $latitude, and longitude

--- Ch13/TravelPhoto.class.php
+++ Ch13/TravelPhoto.class.php
@@ -8,5 +8,15 @@
       private $longitude;
       private $ID;
       public static $photoID = 0;
+    
+    function __construct($fileName, $title, $description, $latitude, $longitude){
+			$this->fileName = $fileName;
+			$this->title = $title;
+      $this->description = $description;
+			$this->latitude = $latitude;
+			$this->longitude = $longitude;
+			self::$photoID++;
+		}
+    
   }
 ?>
