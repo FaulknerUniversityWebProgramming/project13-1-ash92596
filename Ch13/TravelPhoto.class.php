c6b44362701fe9557ae0e5aad6c4df6b45c65aa2
ash92596 <ashleigh.presley@faulkner.edu>
2018-10-30 16:49:59 -0500

    Step 1
    Creating Travel Photo Class

---
+++ Ch13/TravelPhoto.class.php
@@ -0,0 +1,11 @@
+<?php
+  class TravelPhoto {
+      private $date;
+      private $fileName;
+      private $description;
+      private $title;
+      private $latitude;
+      private $longitude;
+      private $ID;
+  }
+?>
